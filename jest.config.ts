fc9594416567a90adfed917647b9b392b3738011
Joao Duarte <joao.duarte@maxmilhas.com.br>
2022-04-12 22:38:20 -0300

    chore: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!./src/**/index.ts',
+    '!./src/**/(main|apm|express|open-api).ts',
+  ],
+  coverageDirectory: './coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
